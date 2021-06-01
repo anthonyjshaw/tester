eb9de1e0b58acea2fd23285e8e8cdee62c5d8c1d
ahmed negm <ahmednegm86@gmail.com>
2021-06-01 20:46:59 +0200

    added test

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,29 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 p 'Destroying records'
 
-User.delete_all
 Project.delete_all
+User.delete_all
 
 p 'Creating user'
 user = User.create!(email: 'user@user.io', password: 123456)
 
 p 'Creating Project'
 
-Project.create!(
+project = Project.create!(
   name: "Tester",
   description: "Tester is the world’s leading community for people to share, test and grow their products",
   github_url: "https://github.com/speakinginlungs/tester",
   user_id: user.id,
 )
 
-p "#{User.count} user created, #{Project.count} project created"
+test = Test.create!(
+  name: "Tester project UX",
+  description: "testing user pain-points",
+  test_url: "http://example.com/acoustics/birthday.html#bedroom",
+  sample_size: 235,
+  time_limit: Date.current.tomorrow,
+  project_id: project.id
+)
+
+
+p "#{User.count} user created, #{Project.count} project created, #{Test.count} test created"
