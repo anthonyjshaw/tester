b50c177b5cb66a962af2aed4d7be6f4e9041831c
ahmed negm <ahmednegm86@gmail.com>
2021-06-01 20:28:53 +0200

    seed boilerplate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-p 'Cleaning db'
+p 'Destroying records'
 
 User.delete_all
 Project.delete_all
@@ -16,9 +16,10 @@ user = User.create!(email: 'user@user.io', password: 123456)
 p 'Creating Project'
 
 Project.create!(
-  name: 'Tester',
-  description: 'Tester is the world’s leading community for people to share, test and grow their products'
-  user_id: user.id
+  name: "Tester",
+  description: "Tester is the world’s leading community for people to share, test and grow their products",
+  github_url: "https://github.com/speakinginlungs/tester",
+  user_id: user.id,
 )
 
-p "#{User.count" user created, #{Project.count} project created"
+p "#{User.count} user created, #{Project.count} project created"
