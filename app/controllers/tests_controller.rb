71efa571e21803e355edcc13a853989d00d35c8e
Anthony Shaw <anthonyshaw@sceaga.com>
2021-06-01 11:46:13 +0100

    Added policy to Test

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,8 +1,18 @@
 class TestsController < ApplicationController
-skip_before_action :authenticate_user!, only: %i[index show]
+  skip_before_action :authenticate_user!, only: %i[index show]
+  before_action :set_test, only: :show
+
   def index
+    @tests = policy_scope(Test)
   end
 
   def show
+    @test = Test.find(params[:id])
+  end
+
+  private
+
+  def set_test
+    @test = Test.find(params[:id])
   end
 end
