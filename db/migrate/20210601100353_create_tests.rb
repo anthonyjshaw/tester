07137cbb4ba2d804ac0a3a6d1a74d83da6d4fed1
Anthony Shaw <anthonyshaw@sceaga.com>
2021-06-01 11:12:50 +0100

    Added example

--- db/migrate/20210601100353_create_tests.rb
+++ db/migrate/20210601100353_create_tests.rb
@@ -2,7 +2,7 @@ class CreateTests < ActiveRecord::Migration[6.0]
   def change
     create_table :tests do |t|
       t.references :project, null: false, foreign_key: true
-      t.boolean :is_finished
+      t.boolean :is_finished, default: false
       t.string :name
       t.text :description
       t.string :test_url
