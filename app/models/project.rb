9eae27de4299708aaf6bc7dc03858123e3cf34a0
Anthony Shaw <anthonyshaw@sceaga.com>
2021-06-02 18:41:35 +0100

    Added files

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,6 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :tests, dependent: :destroy
   validates :name, :description, :github_url, presence: true
-  validates :project_tag, inclusion: { in: "Animation", "Branding", "Illustration", "Mobile", "Print", "Product-Design", "Typography", "Web-Design" }
-
+  validates :project_tag, inclusion: { in: ["Animation", "Branding", "Illustration", "Mobile", "Print", "Product-Design", "Typography", "Web-Design"] }
 
 end
