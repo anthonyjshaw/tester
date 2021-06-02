5cda416e890adc4037f452e2bc6a1176b820ec5a
Anthony Shaw <anthonyshaw@sceaga.com>
2021-06-02 10:18:05 +0100

    Added validations to Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :test
+
+  validates_presence_of :content, :rating, :question1, :question2, :question3
+  validates_numericality_of :rating
 end
