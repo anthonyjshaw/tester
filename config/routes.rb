5a7fae5db38423423401a66f1b6ae959b83ccb17
ahmed negm <ahmednegm86@gmail.com>
2021-06-03 14:17:30 +0200

    removed feed route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  get '/feed', to: 'projects#index', as: :feed
   get '/my-projects', to: 'projects#user_index', as: :my_projects
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
