c73b71dd82d5027c96784e2d51489bfb4f7737d2
ahmed negm <ahmednegm86@gmail.com>
2021-06-03 12:33:16 +0200

    added feed page and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  get '/feed', to: 'projects#index', as: :feed
   get '/my-projects', to: 'projects#user_index', as: :my_projects
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
   resources :projects do
     resources :tests, only: %i[new create index]
   end
-  
+
   resources :tests, only: %i[index show edit update] do
     resources :reviews, only: %i[index create new]
   end
