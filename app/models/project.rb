46eab5415b5cf772f560fdbeb28431e5cf813857
Jan Peter <masteryourownway@gmail.com>
2021-06-03 11:59:41 +0200

    chechin 12 am

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,17 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :tests, dependent: :destroy
   validates :name, :description, :github_url, presence: true
-  validates :project_tag, inclusion: { in: "Animation", "Branding", "Illustration", "Mobile", "Print", "Product-Design", "Typography", "Web-Design" }
+  validates_inclusion_of :project_tag, in: @allowed_types
 
+  @allowed_types => {
+    "Animation",
+     "Branding",
+      "Illustration",
+       "Mobile",
+        "Print",
+         "Product-Design",
+          "Typography",
+           "Web-Design"
+  }
 
 end
