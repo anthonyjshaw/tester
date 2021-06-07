1bf5721b9b7d97f0a3c8e6715b0a6319ef70b421
Anthony Shaw <anthonyshaw@sceaga.com>
2021-06-07 18:43:44 +0100

    Changed cover image colour

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,8 +1,18 @@
 class ChatroomsController < ApplicationController
   def index
     @chatrooms = policy_scope(Chatroom).where('sender_id = ? or receiver_id = ?', current_user.id, current_user.id)
+    @chatroom = Chatroom.new
+  end
 
-
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+    @chatroom.sender_id = current_user
+    @chatroom.receiver_id = User.find(29)
+    if @chatroom.save
+      redirect_to my_chatrooms_path
+    else
+      render :index
+    end
   end
 
   def show
@@ -10,5 +20,11 @@ class ChatroomsController < ApplicationController
     @message = Message.new
     authorize @chatroom
     authorize @message
+  end
+
+  private
+
+  def chatroom_params
+    params.require(:chatroom).permit(:name)
   end
 end
