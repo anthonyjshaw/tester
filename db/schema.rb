98f385e52940c9cd40e7e3adf599ab69ff372667
Jan Peter <masteryourownway@gmail.com>
2021-06-08 18:15:07 +0200

    Added photo upload function to Users profil

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_07_122710) do
+ActiveRecord::Schema.define(version: 2021_06_08_160210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,9 +48,13 @@ ActiveRecord::Schema.define(version: 2021_06_07_122710) do
     t.string "content"
     t.bigint "chatroom_id", null: false
     t.bigint "user_id", null: false
+    t.bigint "sender_id"
+    t.bigint "receiver_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["receiver_id"], name: "index_messages_on_receiver_id"
+    t.index ["sender_id"], name: "index_messages_on_sender_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
@@ -115,6 +119,7 @@ ActiveRecord::Schema.define(version: 2021_06_07_122710) do
     t.string "username"
     t.string "provider"
     t.string "uid"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -122,6 +127,8 @@ ActiveRecord::Schema.define(version: 2021_06_07_122710) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "messages", "users", column: "receiver_id"
+  add_foreign_key "messages", "users", column: "sender_id"
   add_foreign_key "projects", "users"
   add_foreign_key "reviews", "tests"
   add_foreign_key "reviews", "users"
